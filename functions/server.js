73bb1d40f2fb37f1d21d361618f27296d930c4e0
Marina <80592305+marivoid@users.noreply.github.com>
2025-01-14 00:19:56 +0100

    Update server.js

--- functions/server.js
+++ functions/server.js
@@ -6,8 +6,7 @@ const middlewares = jsonServer.defaults();
 server.use(middlewares);
 server.use(router);
 
-const handler = server.listen(3000, () => {
-  console.log("JSON Server is running");
+const PORT = process.env.PORT || 3000; // Use Render's assigned PORT or default to 3000 locally
+server.listen(PORT, () => {
+  console.log(`JSON Server is running on port ${PORT}`);
 });
-
-module.exports = handler;
